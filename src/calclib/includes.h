8333faf84005e1db154ab60f46b7a990fb52cbd2
nique769 <nique769>
2003-08-18 13:53:55 +0000

    *** empty log message ***

--- src/calclib/includes.h
+++ src/calclib/includes.h
@@ -48,7 +48,7 @@ typedef enum {
 #define SIGN_POWER_3		"³"
 #define SIGN_EURO		"€"
 #define SIGN_MICRO		"µ"
-#define SIGN_PI			"Π"
+#define SIGN_PI			"π"
 #define SIGN_MULTIPLICATION	"×"
 #define SIGN_MULTIDOT		"⋅"
 #define SIGN_DIVISION		"∕"
