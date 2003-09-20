3deae679c9b11a53a35e2bc6800b4f9ba0bf6597
nique769 <nique769>
2003-09-20 15:28:27 +0000

    *** empty log message ***

---
+++ src/calclib/qalculate.h
@@ -0,0 +1,31 @@
+/*
+    Qalculate    
+
+    Copyright (C) 2003  Niklas Knutsson (nq@altern.org)
+
+    This program is free software; you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation; either version 2 of the License, or
+    (at your option) any later version.
+*/
+
+#ifndef QALCULATE_H
+#define QALCULATE_H
+
+#include "includes.h"
+#include "util.h"
+#include "Calculator.h"
+#include "Manager.h"
+#include "Integer.h"
+#include "Fraction.h"
+#include "Matrix.h"
+#include "Prefix.h"
+#include "EqItem.h"
+#include "ExpressionItem.h"
+#include "Variable.h"
+#include "Function.h"
+#include "Unit.h"
+#include "BuiltinFunctions.h"
+#include "Error.h"
+
+#endif
